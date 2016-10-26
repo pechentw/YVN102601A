8658eb98e44a218229ea95e3dbdb2eeebe6b4e8a
pechentw <pechen.tw@gmail.com>
2016-10-26 19:56:14 +0800

    spinner action methods 1) selected, action at once 2) click the button, then action

--- app/src/main/java/com/soho/yvtc/yvn102601a/MainActivity.java
+++ app/src/main/java/com/soho/yvtc/yvn102601a/MainActivity.java
@@ -11,15 +11,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Spinner spinner;
-    TextView tv2;
+    TextView tv2,tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv2= (TextView) findViewById(R.id.textView2);
+        tv3= (TextView) findViewById(R.id.textView3);
         spinner=(Spinner) findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() { //選了就立刻更新
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 //                TextView t = (TextView) view;
@@ -40,5 +41,12 @@ public class MainActivity extends AppCompatActivity {
         EditText ed = (EditText)findViewById(R.id.editText);
         String str = ed.getText().toString();
         Log.d("MYLOG", str);
+    }
+    public void click2(View v) ////方法2 選了ok 按鈕(click2)才更新
+    {
+        int position = spinner.getSelectedItemPosition();
+        String[] arr = getResources().getStringArray(R.array.fruits);
+        String str = arr[position];
+        tv3.setText(str);
     }
 }
